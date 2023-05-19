37d4205ab19f0039b1fc52eff85a1ea8472ac2c0
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-05-19 13:07:38 +0100

    commit ig?

--- wiki_func.js
+++ wiki_func.js
@@ -1,13 +1,14 @@
 
+
 window.addEventListener('scroll',() =>{
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
-  if (window.pageYOffset >= 0) {
+  if (window.pageYOffset > 0) {
     navbar.classList.add("sticky")
     console.log(window.pageYOffset)
   }
   else {
-    navbar.classList.remove("sticky");
+    navbar.classList.add("sticky");
     console.log('top')
   }
 })
