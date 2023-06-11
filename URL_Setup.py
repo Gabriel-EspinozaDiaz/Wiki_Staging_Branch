1416273ff00cf980ac470929bd181c19e950cdc8
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-06-11 18:33:52 +0100

    nvm

--- URL_Setup.py
+++ URL_Setup.py
@@ -1,3 +1,3 @@
 import copier
 
-copier.run_copy("gh:igembitsgoa/igem-wiki-starter", "Users/gabriele/Desktop/Edin_iGEM_Wiki")
+copier.run_copy("gh:igembitsgoa/igem-wiki-starter", "/Users/gabriele/Desktop/Junior Year/Igem/Wiki_Proj_Logo")
