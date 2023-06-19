71ac57e20cd8f0b864ba1797028d057033df232b
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-06-19 09:46:09 +0100

    Imports

--- URL_Setup.py
+++
@@ -1,3 +0,0 @@
-import copier
-
-copier.run_copy("gh:igembitsgoa/igem-wiki-starter", "/Users/gabriele/Desktop/Junior Year/Igem/Wiki_Proj_Logo")
--- wiki_func.js
+++ wiki_func.js
@@ -3,9 +3,9 @@
 window.addEventListener('scroll',() =>{
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
-  if (window.pageYOffset > 0) {
+  if (window.scrollY > 0) {
     navbar.classList.add("sticky")
-    console.log(window.pageYOffset)
+    console.log(window.scrollY)
   }
   else {
     navbar.classList.add("sticky");
