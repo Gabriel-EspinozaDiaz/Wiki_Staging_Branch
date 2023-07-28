147f7231b1b6f05abb027500d6300d189979171f
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-07-28 11:14:09 +0100

    attempt 2

--- wiki_func.js
+++ wiki_func.js
@@ -19,17 +19,41 @@ window.addEventListener('scroll',() =>{
 const gifContainer = document.querySelector('.gif-container');
 const forwardGif = document.querySelector('.forward-gif');
 const backwardGif = document.querySelector('.backward-gif');
+let isHovered = false;
 
-gifContainer.addEventListener('mouseenter', () => {
+function playGifForward() {
+  forwardGif.style.opacity = 1;
+  backwardGif.style.opacity = 0;
+  forwardGif.style.animationPlayState = 'running';
+  backwardGif.style.animationPlayState = 'paused';
+}
+
+function playGifBackward() {
   forwardGif.style.opacity = 0;
   backwardGif.style.opacity = 1;
+  forwardGif.style.animationPlayState = 'paused';
+  backwardGif.style.animationPlayState = 'running';
+}
+
+gifContainer.addEventListener('mouseenter', () => {
+  isHovered = true;
+  playGifForward();
 });
 
 gifContainer.addEventListener('mouseleave', () => {
-  forwardGif.style.opacity = 1;
-  backwardGif.style.opacity = 0;
+  isHovered = false;
+  playGifBackward();
 });
 
+// Check the GIF's state and play backward when not hovered
+function checkGifState() {
+  if (!isHovered) {
+    playGifBackward();
+  }
+  requestAnimationFrame(checkGifState);
+}
+
+requestAnimationFrame(checkGifState);
 
 
 
