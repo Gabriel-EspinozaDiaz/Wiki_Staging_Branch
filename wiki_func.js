051b09780d579784f03b13e0dda1b9a4e84de6a5
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-10-06 20:48:05 +0100

    features: too many for me to count lmao

--- wiki_func.js
+++ wiki_func.js
@@ -25,3 +25,35 @@ imageContainer.addEventListener('mouseenter', () => {
 imageContainer.addEventListener('mouseleave', () => {
   imageContainer.classList.remove('hovered');
 });
+
+
+// 3D Model 
+
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create a WebGL renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Load your CAD model (replace 'model.gltf' with your model file)
+const loader = new THREE.GLTFLoader();
+loader.load('model.gltf', (gltf) => {
+    const model = gltf.scene;
+    scene.add(model);
+});
+
+// Create orbit and pan controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Render the scene
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update(); // Update the controls
+    renderer.render(scene, camera);
+}
+animate();
